08a8c3979550e6d4384ef468a8fbdc29ac8e4d1a
Dushyant Ramful <dushyantramful@gmail.com>
2021-07-27 15:14:56 +0400

    Checkbox implementation+Default value of completed to false

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  # read all tasks
-  get '/tasks',         to: 'tasks#index',  as: :tasks
-  # create a task
-  get   'tasks/new',    to: 'tasks#new',    as: :new_task
-  post  'tasks',        to: 'tasks#create'
-  # update a task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id',    to: 'tasks#update'
-  # remove a task
-  delete 'tasks/:id',   to: 'tasks#destroy'
-  # read 1 task
-  get '/tasks/:id',     to: 'tasks#show', as: :task
+  # # read all tasks
+  # get '/tasks',         to: 'tasks#index',  as: :tasks
+  # # create a task
+  # get   'tasks/new',    to: 'tasks#new',    as: :new_task
+  # post  'tasks',        to: 'tasks#create'
+  # # read 1 task
+  # get '/tasks/:id',     to: 'tasks#show', as: :task
+  # # update a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id',    to: 'tasks#update'
+  # # remove a task
+  # delete 'tasks/:id',   to: 'tasks#destroy'
+  resources :tasks
 end
