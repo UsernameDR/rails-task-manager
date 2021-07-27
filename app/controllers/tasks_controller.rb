4b6941367a3718611e924684365b932691574d92
Dushyant Ramful <dushyantramful@gmail.com>
2021-07-27 12:47:08 +0400

    create function implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,21 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # read all tasks
   get '/tasks',     to: 'tasks#index',  as: :tasks
+  # create a task
+  get   'tasks/new',  to: 'tasks#new', as: :new_task
+  post  'tasks',      to: 'tasks#create'
   # read 1 task
   get '/tasks/:id', to: 'tasks#show',   as: :task
 end
