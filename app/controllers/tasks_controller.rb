603ebc4e7ce7de78cae04de59c1283dcd1e1441b
Dushyant Ramful <dushyantramful@gmail.com>
2021-07-27 13:11:34 +0400

    implemented update function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,17 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    # no need for app/views/restaurants/create.html.erb
+    # no need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
     redirect_to task_path(@task)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # read all tasks
-  get '/tasks',     to: 'tasks#index',  as: :tasks
+  get '/tasks',         to: 'tasks#index',  as: :tasks
   # create a task
-  get   'tasks/new',  to: 'tasks#new', as: :new_task
-  post  'tasks',      to: 'tasks#create'
+  get   'tasks/new',    to: 'tasks#new',    as: :new_task
+  post  'tasks',        to: 'tasks#create'
+  # update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id',    to: 'tasks#update'
   # read 1 task
-  get '/tasks/:id', to: 'tasks#show',   as: :task
+  get '/tasks/:id',     to: 'tasks#show', as: :task
 end
