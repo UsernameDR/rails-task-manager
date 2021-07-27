1f9046b87d1e163bd91a546e1576de96c6ad028f
Dushyant Ramful <dushyantramful@gmail.com>
2021-07-27 12:24:11 +0400

    created index page and show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # read all tasks
+  get '/tasks',     to: 'tasks#index',  as: :tasks
+  # read 1 task
+  get '/tasks/:id', to: 'tasks#show',   as: :task
 end
